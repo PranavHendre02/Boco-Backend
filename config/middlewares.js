7d83f1262f6f7669567bffcd18d91672c3e12682
Pranav <pranavhendre07@gmail.com>
2025-07-10 01:09:43 +0530

    changes in middleware.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,28 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'https://bocoapp.netlify.app'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://bocoapp.netlify.app'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5173', 'https://bocoapp.netlify.app'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
